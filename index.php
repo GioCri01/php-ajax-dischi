19d7073df0c361697a6abbe9a06b8a1af3c2cebf
GioCri01 <giovanni.crispino94@icloud.com>
2022-06-09 16:35:05 +0200

    MileStone 1

--- index.php
+++ index.php
@@ -15,10 +15,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
   </head>
     <title>Document</title>
 </head>
 <body>
-    
+    <header></header>
+    <main>
+        <div class="container d-flex flex-wrap">
+            <?php foreach($dischi as $key ):?>
+            <div class="box">
+                <img src="<?php echo $key["poster"]?>" alt="">
+                <h4><?php echo $key["title"]?></h4>
+                <h4><?php echo $key["author"]?></h4>
+                <h4><?php echo $key["genre"]?></h4>
+                <h4><?php echo $key["year"]?></h4>
+            </div>
+            <?php endforeach;?>
+        </div>
+    </main>
 </body>
-</html>
+</hh4
